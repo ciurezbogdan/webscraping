cd87eb8b6faea5d58a8a3faf58736803171f3258
_ciurez_bogdan <ciurezbogdan@hotmail.com>
2023-01-18 12:31:52 +0200

    webscraping exercise

--- handlers/data.py
+++ handlers/data.py
@@ -6,6 +6,9 @@ hsl_keys = ('h', 's', 'l')
 
 
 def parse_colors(soup):
+    """ create list with color codes hex, rgb and hsl format
+     :param soup: source page
+     :returns colors: list of color codes"""
     colors = []
 
     # get chart element by ID
@@ -25,11 +28,18 @@ def parse_colors(soup):
 
 
 def get_color_data(string, keys):
+    """ function to create dictionary with color codes
+    :param string: color codes from source page
+    :param keys: hex,rgb or hsl
+    :returns : color codes dict"""
     values = get_values_from_string(string)
     return {key: value for key, value in zip(values, keys)}
 
 
 def get_values_from_string(data):
+    """ matches filter type of color codes
+    :param data: page source
+    :returns: list with color codes values"""
     result = []
     pattern = r'[0-9]+%?'
     matches = re.findall(pattern, data)
@@ -39,6 +49,10 @@ def get_values_from_string(data):
 
 
 def write_output(target, data):
+    """ creates json file output
+     :param target: html source
+     :param data: color codes list
+     """
     data.append(target)
     with open('output_data.json', 'w') as json_file:
         json_object = json.dumps(data)
